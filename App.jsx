10ae15052ab1606e8421f23f6b2847f5696bc9e8
vdpoljoost-rgb <vdpol.joost@gmail.com>
2025-08-22 14:57:48 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{backgroundColor:'#0a0a0a',color:'white',height:'100vh',display:'flex',alignItems:'center',justifyContent:'center'}}>
+      <h1>High Intensity Training by Joost</h1>
+    </div>
+  );
+}
